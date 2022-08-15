78937b2dd485d761254fb714ec749b5c13156397
najongjine <najongjin3@hotmail.com>
2022-08-15 12:35:41 +0900

    .

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+
+    private bool isGrounded;
+    private string GROUND_TAG = "Ground";
+
+    private string ENEMY_TAG = "Enemy";
+
+    private void Awake()
+    {
+
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+    }
+    void PlayerMoveKeyboard()
+    {
+
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
+
+    }
+
+}
